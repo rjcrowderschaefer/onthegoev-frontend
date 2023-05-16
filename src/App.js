9a6314cfea67f19ed2cfd08d7d12fbb689ffe585
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-05-16 19:02:56 -0400

    Created front end components & page structure and set up primary routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>
+          <a href="/">On The Go! EV</a>
+        </h1>
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path='/' />
+          <Route path='/top-evs-2023' />
+          <Route path='/before-you-go' />
+          <Route path='/trip-planner' />
+          <Route path='/charging-locations' />
+          <Route path='/about' />
+          <Route path='/contact' />
+        </Routes>
+      </main>
+    </>
   );
 }
 
