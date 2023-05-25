7295d1319e9a5166b3a3e2b015c1eff7609411d8
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-05-24 21:51:29 -0400

    Made adjustments to homepage css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,8 @@ function Home() {
       <h2 id="h2-hero">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero mauris, dictum nec tristique vel, faucibus at tellus. Vivamus non lacinia justo. Pellentesque eget ex accumsan, egestas nulla ac, dictum metus. </h2>
       <img className="hero-image" src="https://i.imgur.com/vwhyKP2.png"></img></div>
       <div className='home-fact1'>
-        <span id='fact1'>130,000+</span><br />public charging stations in U.S.
+        <div id='fact1'>130,000+</div>
+        <br />public charging stations in U.S.
         </div>
       <div className='home-fact2'>$5 avg spend for 100+ miles of charge range</div>
       <div className='home-fact3'>80% battery for 30 minutes of charging</div>
