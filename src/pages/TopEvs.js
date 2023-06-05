23db766c64aa5937a2b6000853541b1e142f41a9
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-06-05 19:26:16 -0400

    Updated layout and design to top evs page

--- src/pages/TopEvs.js
+++ src/pages/TopEvs.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { carsLoader } from '../apiCalls';
+import { Col, Divider, Row } from 'antd';
 
 function TopEvs() {
     const [carInfo, setCarInfo] = useState([]);
@@ -17,18 +18,52 @@ function TopEvs() {
 }, []);
 
 
-    return carInfo.map((car) => (
-        <div className="cars">
-            <img className="car-image" src={car.img1} alt={car.make} />
-            <h2 className="car-header">{car.year} {car.make} {car.model}</h2>
-            <p className="car-review">{car.review} <a href={`top-evs-2023/${car._id}`}>Read more</a></p>
-            <h6 className="car-score">{car.score}/10 score</h6>
-            <h6 className="car-msrp">{car.msrpStart} - {car.msrpEnd}</h6>
-            <h6 className="car-mileage">{car.mpgCity} City / {car.mpgHwy} Hwy</h6>
-        </div>
-    )
+    // return carInfo.map((car) => (
+    //     <div className="cars">
+    //         <div className="car-card">
+    //         <img className="car-image" src={car.img1} alt={car.make} />
+    //         <h2 className="car-header">{car.year} {car.make} {car.model}</h2>
+    //         <p className="car-review">{car.review} <a href={`top-evs-2023/${car._id}`}>Read more</a></p>
+    //         <h6 className="car-score">{car.score}/10 score</h6>
+    //         <h6 className="car-msrp">{car.msrpStart} - {car.msrpEnd}</h6>
+    //         <h6 className="car-mileage">{car.mpgCity} City / {car.mpgHwy} Hwy</h6>
+    //         </div>
+    //     </div>
+    // )
         
-    );
+    // );
+
+    return (
+        <>
+        <div className="cars">
+            <h2 className="page-title">Best Electric Vehicles of 2023</h2>
+            <p className="page-intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere at leo sit amet tempus. Donec ac volutpat lacus. Sed varius nibh nec dui viverra mollis. Fusce mattis nunc non aliquet rhoncus. </p>
+            <Row gutter={[75, 16]} justify="left" alidn="top">
+                {
+                    carInfo.map(car => {
+                        return (
+                            <Col span={8}>
+                            <div className="car-card">
+                                <div className="car-image">
+                                    <img src={car.img1} alt={car.make} />
+                                </div>
+                            <h2 className="car-header"><b>#{car.rank}</b> {car.year} {car.make} {car.model}</h2>
+                            <p className="car-review">{car.review} <a href={`top-evs-2023/${car._id}`}>Read more</a></p>
+                            <div className="car-card-details">
+                                <h6 className="car-score"><b>{car.score}</b> <div className="card-label">score</div></h6>
+                                <h6 className="car-msrp"><b>{car.msrpStart}</b> - <b>{car.msrpEnd}</b> <div className="card-label">MSRP</div></h6>
+                                <h6 className="car-mileage"><b>{car.mpgCity} City / {car.mpgHwy} Hwy</b> <div className="card-label">MPGe</div></h6>
+                            </div>
+                            </div>
+                        </Col>
+                        )
+                    })
+                }
+            </Row>
+        </div>
+        </>
+     )
 }
+
 
 export default TopEvs;
