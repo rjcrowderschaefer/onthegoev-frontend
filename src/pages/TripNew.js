4d391887dbbd55843e254c2721bcf79c3fcce07c
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-06-07 08:47:35 -0400

    Updated design and layout of add new trip form and page

--- src/pages/TripNew.js
+++ src/pages/TripNew.js
@@ -62,39 +62,90 @@ function CreateTrip() {
 
     return(
         <>
+            <div className="add-new-trip">
+            <h2 className="page-title">Add a new trip</h2>
+            <p className="page-intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere at leo sit amet tempus. Donec ac volutpat lacus. Sed varius nibh nec dui viverra mollis. Fusce mattis nunc non aliquet rhoncus.</p>
             <form onSubmit={handleSubmit}>
-                <label>Trip Name:</label>
-                <input type="text" name="tripName" onChange={handleChange} placeholder="Trip name"/>
-                <label>Trip Owner:</label>
-                <input type="text" name="tripOwner" onChange={handleChange} placeholder="Trip owner"/>
-                <label># Travelers:</label>
-                <input type="number" name="numTravelers" onChange={handleChange} placeholder="Num of travelers"/>
-                <label>Start Date:</label>
-                <input type="text" name="tripStart" onChange={handleChange} placeholder="Start date of trip"/>
-                <label>End Date:</label>
-                <input type="text" name="tripEnd" onChange={handleChange} placeholder="End date of trip"/>
-                <label>Car Year:</label>
-                <input type="number" name="carYear" onChange={handleChange} placeholder="Car year"/>
-                <label>Car Make:</label>
-                <input type="text" name="carMake" onChange={handleChange} placeholder="Car make"/>
-                <label>Car Model:</label>
-                <input type="text" name="carModel" onChange={handleChange} placeholder="Car model"/>
-                <label>Start Point:</label>
-                <input type="text" name="startPoint" onChange={handleChange} placeholder="Trip starting location"/>
-                <label>End Point:</label>
-                <input type="text" name="endPoint" onChange={handleChange} placeholder="Trip end location"/>
-                <label>Total Trip Distance:</label>
-                <input type="number" name="totalDistance" onChange={handleChange} placeholder="Total distance of trip"/>
-                <label>Total MPC Hwy:</label>
-                <input type="number" name="distanceHwy" onChange={handleChange} placeholder="Total miles per charge (hwy)"/>
-                <label>Total Charges:</label>
-                <input type="number" name="numStops" onChange={handleChange} placeholder="Number of stops to charge"/>
-                <label>Notes:</label>
-                <input type="text" name="notes" onChange={handleChange} placeholder="Add trip notes here" />
-                <label>Image:</label>
-                <input type="text" name="img" onChange={handleChange} placeholder="Add trip image" />
-                <button>Create Trip</button>
+                
+                <div className="form-group">
+                    <div class="row">
+                        <div class="col">
+                            <label for="tripName">Trip Name</label>
+                            <input type="text" class="form-control" name="tripName" onChange={handleChange} placeholder="Where are you headed?"/>
+                        </div>
+                        <div class="col">
+                            <label for="tripOwner">Trip Owner:</label>
+                            <input type="text" class="form-control" name="tripOwner" onChange={handleChange} placeholder="Main point of contact"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label># Travelers:</label>
+                            <input type="number" class="form-control" name="numTravelers" onChange={handleChange} placeholder="How many travelers"/>
+                        </div>
+                        <div class="col">
+                            <label>Start Date:</label>
+                            <input type="text" class="form-control" name="tripStart" onChange={handleChange} placeholder="Start date of trip"/>
+                        </div>
+                        <div class="col">
+                            <label>End Date:</label>
+                            <input type="text" class="form-control" name="tripEnd" onChange={handleChange} placeholder="End date of trip"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label>Car Year:</label>
+                            <input type="number" class="form-control" name="carYear" onChange={handleChange} placeholder="Car year"/>
+                        </div>
+                        <div class="col">
+                            <label>Car Make:</label>
+                            <input type="text" class="form-control" name="carMake" onChange={handleChange} placeholder="Car make"/>
+                        </div>
+                        <div class="col">
+                            <label>Car Model:</label>
+                            <input type="text" class="form-control" name="carModel" onChange={handleChange} placeholder="Car model"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label>Start Point:</label>
+                            <input type="text" class="form-control" name="startPoint" onChange={handleChange} placeholder="Trip starting location"/>
+                        </div>
+                        <div class="col">
+                            <label>End Point:</label>
+                            <input type="text" class="form-control" name="endPoint" onChange={handleChange} placeholder="Trip end location"/>
+                        </div>
+                        <div class="col">
+                            <label>Total Trip Distance:</label>
+                            <input type="number" class="form-control" name="totalDistance" onChange={handleChange} placeholder="Total distance of trip"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                       
+                        <div class="col">
+                            <label>Total MPC Hwy:</label>
+                            <input type="number" class="form-control" name="distanceHwy" onChange={handleChange} placeholder="Total miles per charge (hwy)"/>
+                        </div>
+                        <div class="col">
+                            <label>Total Charges:</label>
+                            <input type="number" class="form-control" name="numStops" onChange={handleChange} placeholder="Number of stops to charge"/>
+                        </div>
+                        <div class="col">
+                            <label>Image:</label>
+                            <input type="file" class="form-control" name="img" onChange={handleChange} placeholder="Add trip image" />
+                        </div>
+                    <div class="row">
+                        
+                        </div>
+                        <div class="col">
+                            <label for="notes">Notes:</label>
+                            <textarea class="form-control" name="notes" rows="3" onChange={handleChange} placeholder="Add trip notes here" />
+                        </div>
+                    </div>
+                </div><br />
+                <button type="submit" class="btn btn-primary">Create Trip</button>
             </form>
+        </div>
         </>
     )
 
